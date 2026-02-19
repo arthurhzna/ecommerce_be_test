dfa29b0ef8ca16e84cbf6771f4efbd626c8324b6
arthurhzna <arthurhozana123@gmail.com>
2026-02-19 23:22:22 +0700

    feat: fix config init

--- config/config.go
+++ config/config.go
@@ -1,4 +1,7 @@
+package config
+
 import (
+	"log"
 	"os"
 	"strconv"
 
@@ -30,6 +33,56 @@ type Database struct {
 }
 
 func Init() {
-	config := AppConfig{
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file loaded:", err)
+	}
 
+	Config = AppConfig{
+		Port:   getEnvAsInt("APP_PORT", 8080),
+		ApiKey: os.Getenv("API_KEY"),
+		Database: Database{
+			Host:                  getEnv("DB_HOST", "127.0.0.1"),
+			Port:                  getEnvAsInt("DB_PORT", 5432),
+			Name:                  getEnv("DB_NAME", "ecommerce"),
+			Username:              getEnv("DB_USERNAME", "postgres"),
+			Password:              getEnv("DB_PASSWORD", "postgres"),
+			MaxOpenConnections:    getEnvAsInt("DB_MAX_OPEN_CONNS", 25),
+			MaxLifeTimeConnection: getEnvAsInt("DB_MAX_LIFE_TIME_SEC", 300),
+			MaxIdleConnections:    getEnvAsInt("DB_MAX_IDLE_CONNS", 5),
+			MaxIdleTime:           getEnvAsInt("DB_MAX_IDLE_TIME_SEC", 60),
+		},
+		RateLimiterMaxRequest: getEnvAsFloat("RATE_LIMITER_MAX_REQUEST", 10.0),
+		RateLimiterTimeSecond: getEnvAsInt("RATE_LIMITER_TIME_SECOND", 60),
+		JwtSecretKey:          getEnv("JWT_SECRET_KEY", ""),
+		JwtExpirationTime:     getEnvAsInt("JWT_EXPIRATION_TIME", 3600),
+	}
+}
+
+func getEnv(key string, defaultVal string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultVal
+}
+
+func getEnvAsInt(key string, defaultVal int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return defaultVal
+	}
+	if i, err := strconv.Atoi(v); err == nil {
+		return i
+	}
+	return defaultVal
+}
+
+func getEnvAsFloat(key string, defaultVal float64) float64 {
+	v := os.Getenv(key)
+	if v == "" {
+		return defaultVal
+	}
+	if f, err := strconv.ParseFloat(v, 64); err == nil {
+		return f
+	}
+	return defaultVal
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.11
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/spf13/cobra v1.10.2
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
